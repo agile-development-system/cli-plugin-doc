dda93a21d98801ecc6128511b3b20c0fcce7b06a
jinyang <961284688@qq.com>
2021-04-13 00:21:24 +0800

    feat:项目初始配置

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 锦阳
+ * @Create: 2021年04月12日
+ */
+class A {
+    a=1;
+    b=1;
+    c=1;
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    static func() {
+        return 10;
+    }
+}
+
+export default A;
